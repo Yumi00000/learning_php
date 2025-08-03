6f7f6d2b236251c44e9a919c1a031e31e6c2f790
Yumi <vb34550@gmail.com>
2025-08-03 17:01:57 +0200

    fix: fix csrf token checking on prod

--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -18,23 +18,27 @@ class ContactFormType extends AbstractType
 
         $builder
             ->add('name', TextType::class, ['constraints' => [
-                new NotBlank(['message' => 'Please enter your name.']),],])
+                new NotBlank(message: 'Please enter your name.'),],])
             ->add('email', EmailType::class, ['constraints' => [
-                new NotBlank(['message' => 'Please enter your email address.'])],
+                new NotBlank(message :'Please enter your email address.')],
             ])
             ->add('subject', TextType::class, [
                 'constraints' => [
-                    new NotBlank(['message' => 'Please enter a subject.']),
+                    new NotBlank(message: 'Please enter a subject.'),
                 ],
             ])
             ->add('message', TextareaType::class, ['constraints' => [
-                new NotBlank(['message' => 'Please enter your message.']),],]);
+                new NotBlank(message: 'Please enter your message.'),],]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
             'data_class' => ContactForm::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id' => 'contact_form',
+            'csrf_cookie' => false, // Disable double-submit cookie
         ]);
     }
 }
